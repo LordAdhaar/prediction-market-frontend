dfa9407c2663baa02dc0a325ec553a82293ddeac
Adhaar Jain <adhaarjain@Adhaars-MacBook-Air.local>
2025-01-06 23:49:22 +0530

    remove unnecessary shadcn components

--- constants/contracts.ts
+++ constants/contracts.ts
@@ -2,8 +2,8 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { sepolia } from "thirdweb/chains";
 
-export const predictionMarketContractAddress = "0x1996d02bb098837ad25c1a6ffe44da9ea0fbb1fd";
-export const tokenContractAddress = "0xecc0933130ae3136cbec878b98bb00983f15fd1a";
+export const predictionMarketContractAddress = "0x298ae4684886e02d48cdfc82d75eb5f2c7921637";
+export const tokenContractAddress = "0x08b5245dcf364c5532e779f4e3e6a6f54d4945fa";
 
 export const predictionMarketContract = getContract({
     client: client,
